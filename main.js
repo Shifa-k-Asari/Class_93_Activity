d6b5ec663691df2c537427c503d97d2ae0a0be97
Shifa-k-Asari <90330560+Shifa-k-Asari@users.noreply.github.com>
2021-12-02 16:04:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML="";
+    recognition.start();
+}
+
+recognition.onresult = function(event){
+    console.log(event);
+
+    var content = event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML=content;
+    console.log(content);
+    if (content == "Take my selfie") {
+        console.log("Taking selfie ____");
+        speak();
+    }
+}
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speak_data = "Taking your selfie in 5 seconds";
+    console.log("Hi");
+    var utterthis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterthis);
+
+    Webcam.attach(cam);
+    setTimeout(function(){
+        take_snapshot();
+        save();
+    }, 5000);
+}
+
+Webcam.set({
+    width:360,
+    height:250,
+    img_format:'png',
+    png_quality:90
+});
+cam = document.getElementById("camera");
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie_img" src="'+data_uri+'">';
+    });
+}
+
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_img").src;
+    link.href = image;
+    link.click();
+}
